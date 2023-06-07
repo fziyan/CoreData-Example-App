87c0398635be0df8aa2e7854ecbc998471b594dd
Fatihan Ziyan <ziyanfatihan@gmail.com>
2023-06-08 01:40:07 +0300

    add CoreData example

---
+++ NatureBook/SecondViewController.swift
@@ -0,0 +1,77 @@
+//
+//  SecondViewController.swift
+//  NatureBook
+//
+//  Created by Fatihan Ziyan on 6.06.2023.
+//
+
+import UIKit
+import CoreData
+
+class SecondViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var place: UITextField!
+    @IBOutlet weak var year: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTap))
+        imageView.addGestureRecognizer(gestureRecognizer)
+
+    }
+    
+    @objc func imageTap(){
+        
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    //Fotoğraf seçme olayı burda tetikleniyor
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        imageView.image = info [.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+
+        @IBAction func saveClickedButton(_ sender: Any) {
+            
+            // Veri Kaydetme işlemi
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let saveData = NSEntityDescription.insertNewObject(forEntityName: "Gallery", into: context)
+            
+            saveData.setValue(name.text, forKey: "name")
+            saveData.setValue(place.text, forKey: "place")
+            
+            if let year = Int(year.text!){
+                saveData.setValue(year, forKey: "year")
+            }
+            
+            let imagePress = imageView.image?.jpegData(compressionQuality: 0.5) // resim kalitesi yer kaplamaması için yapılıyor.
+            saveData.setValue(imagePress, forKey: "image")
+            saveData.setValue(UUID(), forKey: "id")
+            
+            do{
+                try context.save()
+                print("Success")
+            } catch{
+                print("Error")
+            }
+            
+            NotificationCenter.default.post(name: NSNotification.Name.init(rawValue: "newData"), object: nil)
+            self.navigationController?.popViewController(animated: true)
+            self.navigationController?.topViewController?.dismiss(animated: true)
+        }
+        
+    
+}
--- NatureBook/ViewController.swift
+++ NatureBook/ViewController.swift
@@ -6,14 +6,83 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var nameArray = [String]()
+    var idArray = [UUID]()
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+    }
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addItem))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        getData()
+        
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name(rawValue: "newData"), object: nil)
+    }
+    
+    // Core data ile veri çekme işlemlerini yapan fonksiyon
+    @objc func getData() {
+        self.nameArray.removeAll(keepingCapacity: true)
+        self.idArray.removeAll(keepingCapacity: true)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Gallery")
+        fetchRequest.returnsObjectsAsFaults = false // Bu satır Core data içindeki verirler okurken  uygulamaının hızını arttırmaya yarıyor. Apple dokümaında böyle yazıyor.
+        
+        do {
+            
+           let results =  try context.fetch(fetchRequest)
+            for result in results as! [NSManagedObject] {
+                
+                if let name = result.value(forKey: "name") as? String{
+                    self.nameArray.append(name)
+                }
+                
+                if let id = result.value(forKey: "id") as? UUID{
+                    self.idArray.append(id)
+                }
+                
+                self.tableView.reloadData()
+            }
+            
+        }catch{
+            
+        }
+    }
+    
+    @objc func addItem(){
+        
+         let vc = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondVC")
+        vc.modalPresentationStyle = .formSheet
+         self.present(vc, animated: true)
+         
+        
+    }
+    
+    
 }
 
